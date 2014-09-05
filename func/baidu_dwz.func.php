fe62a3534c1bdecbbf14f751c678c8f3b7da9e2e
orvice <orvice@gmail.com>
2014-09-05 21:38:19 +0800

    add query longurl

--- func/baidu_dwz.func.php
+++ func/baidu_dwz.func.php
@@ -9,24 +9,20 @@
 function bd_dwz($type,$url){
     if($type == 1){
         $data=array('url'=>$url);
-        //$data= "url=".$url;
         $short = curl_init();
-
         curl_setopt($short,CURLOPT_URL,"http://dwz.cn/create.php");
         curl_setopt($short,CURLOPT_POST,true);
         curl_setopt($short,CURLOPT_RETURNTRANSFER,true);
         curl_setopt($short,CURLOPT_HEADER,0); //remove head
         curl_setopt($short,CURLOPT_POSTFIELDS,$data); // POST
         //Set Charset
-
-
         //Get Json
         $short_json = curl_exec($short);
+        curl_close($short);
         //$curl_close($short);
         $short_array = json_decode($short_json,true);
         $bk = $short_array['status'];
         //No error
-        //if (!curl_errno($short_json)){
         if (!$bk){
 
             $rt = $short_array['tinyurl'];
@@ -36,8 +32,33 @@ function bd_dwz($type,$url){
         else{
             return 0;
         }
+    }
 
+    else{
+        $data=array('tinyurl'=>$url);
+        $short = curl_init();
+        curl_setopt($short,CURLOPT_URL,"http://dwz.cn/query.php");
+        curl_setopt($short,CURLOPT_POST,true);
+        curl_setopt($short,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($short,CURLOPT_HEADER,0); //remove head
+        curl_setopt($short,CURLOPT_POSTFIELDS,$data); // POST
+        //Set Charset
+        //Get Json
+        $short_json = curl_exec($short);
+        curl_close($short);
+        //$curl_close($short);
+        $short_array = json_decode($short_json,true);
+        $bk = $short_array['status'];
+        //No error
+        if (!$bk){
 
+            $rt = $short_array['longurl'];
+            return $rt;
+        }
+        //if error occur,return 0
+        else{
+            return 0;
+        }
     }
 
 
