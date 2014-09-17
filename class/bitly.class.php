1ea77ff0055724924bc6652f41b763699cb8bc1e
orvice <orvice@gmail.com>
2014-09-17 10:51:47 +0800

    fix

--- class/bitly.class.php
+++ class/bitly.class.php
@@ -10,24 +10,20 @@ class bitly {
 
     // ApiKey
     public $token;
+    public $apikey;
     // crul
     //public $cc;
 
     public $baseurl = "https://api-ssl.bitly.com/v3/shorten";
 
-    function  _construct($apikey){
+    // Edit this apikey
+    function  __construct($apikey="4d4cb354e6d48339a5043f98d08572404cf02628"){
         $this->token = $apikey;
-
-        //Edit this token
-        $owntoken   =  "4d4cb354e6d48339a5043f98d08572404cf02628";
-        if($this->token==NULL){
-            $this->token = $owntoken;
-        }
     }
 
     function srt($url){
         $url     = urlencode($url);
-        $furl = $baseurl.'?access_token='.$token.'&longUrl='.$url;
+        $furl = $this->baseurl.'?access_token='.$this->token.'&longUrl='.$url;
         $short  = curl_init($furl);
         curl_setopt($short,CURLOPT_RETURNTRANSFER,true);
         $short_json = curl_exec($short);
@@ -40,12 +36,12 @@ class bitly {
             return $bk;
         }
         else{
-            return 0;
+            return $this->token;
         }
 
     }
 
-    function _destruct(){
+    function __destruct(){
         //curl_close($this->cc);
     }
 
