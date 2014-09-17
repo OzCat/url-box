3e6130aecafb46eac20f5335c61ac738aa6925a3
orvice <orvice@gmail.com>
2014-09-17 10:26:56 +0800

    update

--- class/bitly.class.php
+++ class/bitly.class.php
@@ -6,21 +6,27 @@
  *
  */
 
-
 class bitly {
 
+    // ApiKey
+    public $token;
+    // crul
+    //public $cc;
+
+    public $baseurl = "https://api-ssl.bitly.com/v3/shorten";
+
     function  _construct($apikey){
-        $token = $apikey;
+        $this->token = $apikey;
+
+        //Edit this token
+        $owntoken   =  "4d4cb354e6d48339a5043f98d08572404cf02628";
+        if($this->token==NULL){
+            $this->token = $owntoken;
+        }
     }
 
     function srt($url){
         $url     = urlencode($url);
-        $baseurl = "https://api-ssl.bitly.com/v3/shorten";
-        //Edit this token
-        $owntoken   =  "4d4cb354e6d48339a5043f98d08572404cf02628";
-        if($token==NULL){
-            $token = $owntoken;
-        }
         $furl = $baseurl.'?access_token='.$token.'&longUrl='.$url;
         $short  = curl_init($furl);
         curl_setopt($short,CURLOPT_RETURNTRANSFER,true);
@@ -40,7 +46,7 @@ class bitly {
     }
 
     function _destruct(){
-        curl_close($short);
+        //curl_close($this->cc);
     }
 
 }
